577f7bd509fdbf99f923935ec75eb60ff8b77360
Purusharth vishnoi <purusharth vishnoi>
2023-11-28 16:53:25 +0530

    primeprob

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log()
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Example usage:
+let num = 13;
+if (isPrime(num)) {
+    console.log(`${num} is a prime number.`);
+} else {
+    console.log(`${num} is not a prime number.`);
+}
